31c0665fe8d60b0aea70f13e960e91186c7b4118
Richard Montgomery <rikermonty@gmail.com>
2017-10-25 12:20:30 -0700

    finished logic

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function Student(first, last, status, plans) {
   this.first = first;
   this.last = last;
   this.enrolled = status;
-  this.future_plans = plans;
+  this.futurePlans = plans;
 }
 
 //Stores the form fill
@@ -22,9 +22,25 @@ function formData(event) { //event that we will trigger
   var first = event.target.first.value;
   var last = event.target.last.value;
   var enrolled = event.target.enrolled.checked;
-  var futurePlans = event.target.future_plans.value;
+  var futurePlans = event.target.future_classes.value;
 
-  data.push(new Student(first, last, enrolled, futureClasses));
- //call create table here
+  data.push(new Student(first, last, enrolled, futurePlans));
+  createTable();
   form.reset();
 }
+
+//build out table
+function createTable() {
+  var row;
+  for(var i = 0 ; i < data.length ; i++) {
+    row = document.createElement('tr');
+    row.innerHTML = '<td>' + data[i].first + '</td>' +
+    '<td>' + data[i].last + '</td>' +
+    '<td>' + data[i].enrolled + '</td>' +
+    '<td>' + data[i].futurePlans + '</td>';
+  }
+
+  table.appendChild(row);
+}
+
+form.addEventListener('submit', formData);
